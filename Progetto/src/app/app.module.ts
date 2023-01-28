37df08f263d6e424d65f934e4e51039ce1691f9d
Marco <marcogiarrusso@gmail.com>
2023-01-28 18:25:31 +0100

    crati corsi pg e corso

--- Progetto/src/app/app.module.ts
+++ Progetto/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './commons/header/header.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { CoursePageComponent } from './courses/course-page/course-page.component';
+import { CourseComponent } from './courses/course/course.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursePageComponent,
+    CourseComponent,
+
   ],
   imports: [
     BrowserModule,
