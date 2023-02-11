4500555eca5c768d8c29190f863835124e98e970
Marco <marcogiarrusso@gmail.com>
2023-02-11 12:48:28 +0100

    pre lez 8 ok pwas

--- Progetto/src/app/courses/course.service.ts
+++ Progetto/src/app/courses/course.service.ts
@@ -45,6 +45,10 @@ export class CourseService {
      catchError(this.handleError));
   }
 
+  getpath(){
+    return path;
+  }
+
   private handleError(error: HttpErrorResponse) {  if (error.error instanceof ErrorEvent) {  
       // A client-side or network error occurred. Handle it accordingly.   
        console.error('An error occurred:', error.error.message); 
